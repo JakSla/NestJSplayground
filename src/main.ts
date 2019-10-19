8503ccecf4ad0bdfdc7e79ad31e9cd27f070fe0a
myflowpl <pedro.blaszczak@gmail.com>
2019-10-19 13:27:11 +0200

    swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { expressApp } from '../express/server';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(
@@ -10,6 +11,18 @@ async function bootstrap() {
   );
 
   app.setGlobalPrefix('api');
+
+  // const config = new ConfigService();
+  const options = new DocumentBuilder()
+    .setTitle('Nest API Example')
+    .setDescription('Przykładowy projekt w Node.js i TypeScript')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .addTag('user')
+    // .addBearerAuth(config.TOKEN_HEADER_NAME, 'header')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
 }
